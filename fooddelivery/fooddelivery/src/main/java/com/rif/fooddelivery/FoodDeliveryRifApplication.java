49b5a2fd7366ff7abdfd53ddd8c13da50af7690d
tlili bechir <tlilibechir321@gmail.com>
2020-05-25 21:32:25 +0100

    Initial commit. Created spring boot project and configure the application properties

---
+++ fooddelivery/fooddelivery/src/main/java/com/rif/fooddelivery/FoodDeliveryRifApplication.java
@@ -0,0 +1,13 @@
+package com.rif.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryRifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryRifApplication.class, args);
+	}
+
+}
---
+++ fooddelivery/fooddelivery/src/test/java/com/rif/fooddelivery/FoodDeliveryRifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rif.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryRifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
